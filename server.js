ee41a9ad3de25dbe647e6b266b7cff3e102b9930
bonetoo <ben_nana1@hotmail.com>
2017-10-11 20:19:29 +0700

    finish

--- server.js
+++ server.js
@@ -5,8 +5,19 @@ const app = express()
 app.set('view engine', 'twig')
 
 const homeController = require('./Controller/home.Controller')
+const menuController = require('./Controller/menu.Controller')
+const restaurantsController = require('./Controller/restaurants.Controller')
+const trickController = require('./Controller/trick.Controller')
+const newsController = require('./Controller/news.Controller')
 
 app.get('/', homeController.index)
+app.get('/home', homeController.index)
+app.get('/menu', menuController.index)
+app.get('/restaurants', restaurantsController.index)
+app.get('/trick', trickController.index)
+app.get('/news', newsController.index)
+
+
 
 app.use('/assets', express.static(__dirname + '/static/assets',{
 	maxAge: 86400000
